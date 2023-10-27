2a7ba7d4debe2229f4b7f00d4a5286529d869f84
samiguntepe <samiguntepe@gmail.com>
2023-10-27 20:38:36 +0300

    finish ex00

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -1,7 +1,28 @@
 #include <iostream>
 
-int main(){
+int main(int argc, char **argv){
 
-    std::cout << "hello";
-    return 0;
+    int i, j;
+
+    if(argc == 1)
+    {
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+        return (0);
+    }
+    i = 1;
+    while (argv[i])
+    {
+        j = 0;
+        while (argv[i][j])
+        {
+            if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+                argv[i][j] -= 32;
+            std::cout << argv[i][j] << std::ends;
+            j++;
+        }
+        i++;
+    }
+    std::cout << "\n";
+    
+    return (0);
 }

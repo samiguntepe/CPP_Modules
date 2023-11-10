183b92cb3fee00a05166ae17f334496d7a44b577
samiguntepe <samiguntepe@gmail.com>
2023-11-11 00:39:45 +0300

    42

---
+++ CPP_01/ex02/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main()
+{
+	std::string str = "HI THIS IS BRAIN";
+	std::string *stringPTR = &str;
+	std::string &stringREF = str;
+
+	std::cout << "The memory address of the String variable: " << &str << std::endl;
+	std::cout << "The memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "The memory address held by stringREF: " << &stringREF << std::endl;
+	std::cout << "\nThe value of the String variable: " << str << std::endl;
+	std::cout << "The value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "The value pointed to by stringREF: " << stringREF << std::endl;
+}
